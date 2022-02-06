3344ac83eac48cdff1772e316896b23a3d451982
ugrsen <93542523+ugrsen@users.noreply.github.com>
2022-02-06 16:20:27 +0530

    Add files via upload
    commit

---
+++ index.js
@@ -0,0 +1,50 @@
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+body{
+    background-color: pink;
+}
+main{
+    margin: 100px auto 0 auto;
+    border: 1px solid rgb(12, 179, 245);
+    width: 500px;
+    text-align: center;
+    background-color: rgb(124, 240, 124);
+}
+h1{
+    text-decoration: underline;
+    color: rgb(3, 12, 15);
+}
+img{
+    height: 200px;
+    border-radius: 50%;
+}
+#author{
+    font-size: 18px;
+    font-weight: 1000;
+}
+p{
+    padding: 10px;
+}
+.btn{
+    margin: 10px;
+}
+button{
+    padding: 5px;
+    background-color:rgb(240, 140, 140);
+    border: none;
+    border-radius: 10px;
+}
+section{
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 100px 20px 0 20px;
+    background-color: white;
+}
+a{
+    text-decoration: none;
+    color: black;
+    margin-top: 200px;
+}
